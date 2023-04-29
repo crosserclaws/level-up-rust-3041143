1edc73c89f42415435e09540b136a23c3c11534b
Lucas Lin <crosserclaws@gmail.com>
2023-04-29 09:00:32 +0000

    Done
    The advanced 3 solution includes advanced 2.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-fn unique(a: Vec<i32>) -> Vec<i32> {
-    todo!()
-}
+// fn unique(mut a: Vec<i32>) -> Vec<i32> {
+//     // a.sort();
+//     a.sort_unstable();
+//     a.dedup();
+//     a
+// }
+
+// HashSet
+// use std::collections::HashSet;
+// fn unique(a: Vec<i32>) -> Vec<i32> {
+//     let mut set: HashSet<i32> = HashSet::new();
+//     let mut ans = vec![];
+//     for x in a {
+//         if !set.contains(&x) {
+//             set.insert(x);
+//             ans.push(x);
+//         }
+//     }
+//     ans
+// }
 
 // advanced 1: use generic types
-// fn unique(a: Vec<T>) -> Vec<T> {
-//     todo!();
+// fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+//     // a.sort();
+//     a.sort_unstable();
+//     a.dedup();
+//     a
 // }
 
 // advanced 2: keep items in order
@@ -13,9 +33,22 @@ fn unique(a: Vec<i32>) -> Vec<i32> {
 // }
 
 // advanced 3: use iterators
-// fn unique(a: Iterable<T>) -> Vec<T> {
-//     todo!();
-// }
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::iter::IntoIterator;
+fn unique<T>(a: impl IntoIterator<Item=T>) -> Vec<T>
+where T: Eq + Hash + Copy
+{
+    let mut set: HashSet<T> = HashSet::new();
+    let mut ans = Vec::new();
+    for x in a {
+        if !set.contains(&x) {
+            set.insert(x);
+            ans.push(x);
+        }
+    }
+    ans
+}
 
 fn main() {
     let input = vec![2, 1, 1];
@@ -25,7 +58,7 @@ fn main() {
 
 #[test]
 fn empty_list() {
-    let input = vec![];
+    let input: Vec<i32> = vec![];
     let expected_output = vec![];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
