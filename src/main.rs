d4cb56087608d9fb3e5f9c07e94cbc9426bc6590
Lucas Lin <crosserclaws@gmail.com>
2023-05-01 13:48:53 +0000

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
+use std::fmt;
 use std::str::FromStr;
+use std::error::Error;
 
 struct Isbn {
     raw: String,
     digits: Vec<u8>,
 }
 
+#[derive(Debug)]
+enum IsbnError {
+    TooLong,
+    TooShort,
+    FailedChecksum,
+    InvalidChar(usize, char),
+}
+
+impl Error for IsbnError {}
+
+impl fmt::Display for IsbnError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ISBN Error: {:?}", self)
+    }
+}
+
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    type Err = IsbnError; // TODO: replace with appropriate type
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        let mut digits: Vec<u8> = Vec::new();
+        for (i, ch) in s.char_indices() {
+            if ch == '-' {
+                continue;
+            }
+            let d: u8 = match ch.to_digit(10) {
+                Some(x) => x as u8,
+                None => return Err(IsbnError::InvalidChar(i, ch))
+            };
+            digits.push(d);
+        }
+        match digits.len() {
+            0..=12 => Err(IsbnError::TooShort),
+            13 => {
+                if calculate_check_digit(&digits) == digits[12] {
+                    Ok(Isbn { raw: s.to_string(), digits })
+                } else {
+                    Err(IsbnError::FailedChecksum)
+                }
+            },
+            _ => Err(IsbnError::TooLong)
+        }
     }
 }
 
@@ -21,7 +60,9 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    const WEIGHTS: [u8; 12] = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+    let sum: u8 = digits.iter().zip(WEIGHTS).map(|(&a, b)| a*b).sum();
+    (10 - (sum % 10)) % 10
 }
 
 fn main() {
